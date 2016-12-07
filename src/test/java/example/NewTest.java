2ff573ad131c2e83b962a004ad21eb07accce385
amcfro <andymac615@live.com>
2016-12-07 13:48:29 +0000

    5

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -18,7 +18,7 @@ public class NewTest {
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Free Selenium Tutorials")); 		
 		}	
-		//Changes4
+		//Changes5
 		@Test				
 		public void testEasy1() {	 
 			String title = driver.getTitle();				 
